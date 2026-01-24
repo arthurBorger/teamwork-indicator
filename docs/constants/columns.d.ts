a96116380d0c6b0ceeba8e6123ad1c417718f359
arthurThorkildsen <arthurbt@stud.ntnu.no>
2026-01-24 18:46:12 +0100

    Fixed bugs

---
+++ docs/constants/columns.d.ts
@@ -0,0 +1,11 @@
+export declare const Columns: {
+    readonly GroupNumber: "Gruppenummer";
+    readonly ID: "ID";
+    readonly Start: "Starttidspunkt";
+    readonly End: "Fullføringstidspunkt";
+    readonly Email: "E-postadresse";
+    readonly Name: "Navn";
+    readonly LastChanged: "Tidspunkt for siste endring";
+};
+export type ColumnName = (typeof Columns)[keyof typeof Columns];
+//# sourceMappingURL=columns.d.ts.map
